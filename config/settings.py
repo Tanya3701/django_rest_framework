22a6a8c84e458408d6b147645a09a4cf9358b574
Tanya3701 <ip-bogomazova@mail.ru>
2026-06-23 22:47:21 +0300

    m4

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,8 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
