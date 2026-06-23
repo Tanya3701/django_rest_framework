bf1755719287dec943d02977609d631d80da54ab
Tanya3701 <ip-bogomazova@mail.ru>
2026-06-23 22:10:38 +0300

    myst

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
